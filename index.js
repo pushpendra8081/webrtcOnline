bcacf12741e28510a710ea4169e559d2c7177440
Manish <pushpendrapal8081@gmail.com>
2024-11-20 19:57:45 +0530

    Your commit message here

--- index.js
+++ index.js
@@ -1,51 +1,68 @@
-let port = process.env.PORT || 5000;
+const http = require("http");
+const socketIo = require("socket.io");
 
-let IO = require("socket.io")(port, {
+// Get the port from the environment variable or use 5000 as default
+const port = process.env.PORT || 5000;
+
+// Create the HTTP server
+const server = http.createServer();
+
+// Initialize socket.io with CORS support
+const IO = socketIo(server, {
   cors: {
-    origin: "*",
+    origin: "*", // Replace "*" with your client domain for better security in production
     methods: ["GET", "POST"],
   },
 });
 
+// Middleware to attach user information from handshake query
 IO.use((socket, next) => {
   if (socket.handshake.query) {
-    let callerId = socket.handshake.query.callerId;
+    const callerId = socket.handshake.query.callerId;
     socket.user = callerId;
     next();
   }
 });
 
+// Handle socket.io events
 IO.on("connection", (socket) => {
-  console.log(socket.user, "Connected");
+  console.log(`${socket.user} Connected`);
   socket.join(socket.user);
 
+  // Handle "makeCall" event
   socket.on("makeCall", (data) => {
-    let calleeId = data.calleeId;
-    let sdpOffer = data.sdpOffer;
-
+    const { calleeId, sdpOffer } = data;
     socket.to(calleeId).emit("newCall", {
       callerId: socket.user,
       sdpOffer: sdpOffer,
     });
   });
 
+  // Handle "answerCall" event
   socket.on("answerCall", (data) => {
-    let callerId = data.callerId;
-    let sdpAnswer = data.sdpAnswer;
-
+    const { callerId, sdpAnswer } = data;
     socket.to(callerId).emit("callAnswered", {
       callee: socket.user,
       sdpAnswer: sdpAnswer,
     });
   });
 
+  // Handle "IceCandidate" event
   socket.on("IceCandidate", (data) => {
-    let calleeId = data.calleeId;
-    let iceCandidate = data.iceCandidate;
-
+    const { calleeId, iceCandidate } = data;
     socket.to(calleeId).emit("IceCandidate", {
       sender: socket.user,
       iceCandidate: iceCandidate,
     });
   });
+
+  // Log disconnection
+  socket.on("disconnect", () => {
+    console.log(`${socket.user} Disconnected`);
+  });
+});
+
+// Start the server
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
