b4a95e257d2a1318b4f8a8934f4aaeff81ca1269
Manish <pushpendrapal8081@gmail.com>
2024-11-25 16:31:59 +0530

    Add your commit message here

--- index.js
+++ index.js
@@ -15,50 +15,90 @@ const IO = socketIo(server, {
   },
 });
 
+// Store active user and host connections
+let users = {};
+let hosts = {};
+
 // Middleware to attach user information from handshake query
 IO.use((socket, next) => {
   if (socket.handshake.query) {
     const callerId = socket.handshake.query.callerId;
+    const userType = socket.handshake.query.userType; // 'user' or 'host'
     socket.user = callerId;
+    socket.userType = userType;
     next();
   }
 });
 
 // Handle socket.io events
 IO.on("connection", (socket) => {
-  console.log(`${socket.user} Connected`);
-  socket.join(socket.user);
+  console.log(`${socket.user} (${socket.userType}) connected`);
+
+  // Add the socket to the correct group (user or host)
+  if (socket.userType === "user") {
+    users[socket.user] = socket.id;
+  } else if (socket.userType === "host") {
+    hosts[socket.user] = socket.id;
+  }
 
   // Handle "makeCall" event
   socket.on("makeCall", (data) => {
     const { calleeId, sdpOffer } = data;
-    socket.to(calleeId).emit("newCall", {
-      callerId: socket.user,
-      sdpOffer: sdpOffer,
-    });
+    const calleeSocket = users[calleeId] || hosts[calleeId];
+
+    if (calleeSocket) {
+      io.to(calleeSocket).emit("newCall", {
+        callerId: socket.user,
+        sdpOffer: sdpOffer,
+      });
+      console.log(`Call request from ${socket.user} to ${calleeId}`);
+    } else {
+      console.log(`Target ${calleeId} not online`);
+    }
   });
 
   // Handle "answerCall" event
   socket.on("answerCall", (data) => {
     const { callerId, sdpAnswer } = data;
-    socket.to(callerId).emit("callAnswered", {
-      callee: socket.user,
-      sdpAnswer: sdpAnswer,
-    });
+    const callerSocket = users[callerId] || hosts[callerId];
+
+    if (callerSocket) {
+      io.to(callerSocket).emit("callAnswered", {
+        callee: socket.user,
+        sdpAnswer: sdpAnswer,
+      });
+      console.log(`Call answered from ${socket.user} to ${callerId}`);
+    } else {
+      console.log(`Target ${callerId} not online`);
+    }
   });
 
   // Handle "IceCandidate" event
   socket.on("IceCandidate", (data) => {
     const { calleeId, iceCandidate } = data;
-    socket.to(calleeId).emit("IceCandidate", {
-      sender: socket.user,
-      iceCandidate: iceCandidate,
-    });
+    const calleeSocket = users[calleeId] || hosts[calleeId];
+
+    if (calleeSocket) {
+      io.to(calleeSocket).emit("IceCandidate", {
+        sender: socket.user,
+        iceCandidate: iceCandidate,
+      });
+      console.log(`ICE candidate from ${socket.user} to ${calleeId}`);
+    } else {
+      console.log(`Target ${calleeId} not online`);
+    }
   });
 
-  // Log disconnection
+  // Handle disconnection
   socket.on("disconnect", () => {
-    console.log(`${socket.user} Disconnected`);
+    console.log(`${socket.user} disconnected`);
+
+    // Remove from users or hosts
+    if (socket.userType === "user") {
+      delete users[socket.user];
+    } else if (socket.userType === "host") {
+      delete hosts[socket.user];
+    }
   });
 });
 
@@ -66,4 +106,3 @@ IO.on("connection", (socket) => {
 server.listen(port, '0.0.0.0', () => {
   console.log(`Server is running on port ${port}`);
 });
-
